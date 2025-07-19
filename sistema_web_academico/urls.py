3d76df78e585879d158e485637430769a4768fd5
cesar augusto castro tello <attrib1986@gmail.com>
2025-07-19 17:21:46 -0500

    Actualizo cambios en las apps

--- sistema_web_academico/urls.py
+++ sistema_web_academico/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views  # ⬅️ Importa las vistas del proyecto
+from django.views.generic import TemplateView
 
 urlpatterns = [
 
@@ -27,3 +27,7 @@ urlpatterns = [
     path('informes/', include('informes.urls')),
     path('inscripciones/', include('inscripciones.urls')),
 ] 
+
+urlpatterns += [
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+]
